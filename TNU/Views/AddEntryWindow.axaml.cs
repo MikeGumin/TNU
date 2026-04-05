ab752297b634eb1638a011cb798132eff39d2973
m.gumin <m.gumin@bars.group>
2026-04-05 16:31:27 +0300

    Исправил работу кнопки создания

--- TNU/Views/AddEntryWindow.axaml.cs
+++ TNU/Views/AddEntryWindow.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using TNU.Models;
 
@@ -11,6 +13,10 @@ public partial class AddEntryWindow : Window
     public AddEntryWindow()
     {
         InitializeComponent();
+        
+        JobEntry entry = new JobEntry();
+
+        DataContext = entry;
     }
 
     private void OkButton_Click(object? sender, RoutedEventArgs e)
@@ -22,5 +28,10 @@ public partial class AddEntryWindow : Window
     private void CancelButton_Click(object? sender, RoutedEventArgs e)
     {
         Close(false);
+    }
+
+    private void StartTimeDefault_OnGotFocus(object? sender, GotFocusEventArgs e)
+    {
+        StartTimeTextBox.Text = DateTime.Now.ToString("HH:mm:ss");
     }
 }
