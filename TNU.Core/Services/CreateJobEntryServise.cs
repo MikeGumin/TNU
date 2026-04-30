79d812fe303241b957f56a6dbc6d29a4ceb73e30
R1nful <83274508+R1nful@users.noreply.github.com>
2026-04-30 17:06:48 +0300

    Время у задач исправлено обратно (#48)

--- TNU.Core/Services/CreateJobEntryServise.cs
+++ TNU.Core/Services/CreateJobEntryServise.cs
@@ -11,7 +11,7 @@ namespace TNU.Core.Services
             var obj = new JobEntryClock();
             obj.Entry.Id = JobId++;
             obj.Entry.JobDate = DateTime.Now;
-            obj.Entry.StartDateTime = DateTime.Now;
+
             obj.Entry.JobName = jobName;
 
             GeneralUpdateTimer.AddEvent(obj);
