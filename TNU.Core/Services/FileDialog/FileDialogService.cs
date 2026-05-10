8aa3cffefcc2f74fb92d051c2a5acc82effc33d6
m.gumin <m.gumin@bars.group>
2026-05-11 02:09:30 +0300

    Реализован импорт регистра наименования

--- TNU.Core/Services/FileDialog/FileDialogService.cs
+++ TNU.Core/Services/FileDialog/FileDialogService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;
+using TNU.Core.Models.Enum;
+using TNU.Core.Repository;
 
 namespace TNU.Core.Services.FileDialog;
 
@@ -38,5 +41,50 @@ public class FileDialogService : IFileDialogService
         });
 
         return file is null ? null : await file.OpenWriteAsync();
+    }
+    
+    /// <inheritdoc />
+    public async Task OpenFileAsync()
+    {
+        var topLevel = _getTopLevel();
+        if (topLevel is null) return;
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Открыть файл",
+            AllowMultiple = false, // Только один файл
+            FileTypeFilter = new[] 
+            { 
+                new FilePickerFileType("Текстовые файлы") { Patterns = new[] { "*.txt" } },
+                new FilePickerFileType("Все файлы") { Patterns = new[] { "*" } }
+            }
+        });
+        
+        if (files.Count >= 1)
+        {
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+
+            string? line;
+
+            while ((line = await streamReader.ReadLineAsync()) is not null)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                
+                var columns = line.Split(':');
+                
+                var jobName = columns[0].Trim();
+                JobNameRepository.JobNameList.Add(new JobTitleEnum(jobName));
+
+                if (columns.Length > 1)
+                {
+                    var code = columns[1].Trim();
+                    if (!string.IsNullOrWhiteSpace(code))
+                    {
+                        JobNameRepository.JobNameCodeList[jobName] = code;    
+                    }  
+                }
+            }
+        }
     }
 }
